fa826c7b4b9bb7bb1ddbc82247e1ce4ccdb2c9d3
Shun Cheung <shunkongcheung@gmail.com>
2020-10-12 11:58:46 -0400

    feat(database connection): updated

--- src/getDbConnection.ts
+++ src/getDbConnection.ts
@@ -1,27 +1,34 @@
-import { createConnection, Connection } from "typeorm";
+import { createConnection, getConnectionManager, Connection } from "typeorm";
 import dotenv from "dotenv";
+
+import * as Entities from "./entities";
 
 const getDbConnection = async () => {
   // read .env file into process.env
   dotenv.config();
 
-  // here createConnection will load connection options from
+  // .env has the highest priority, here createConnection will load connection options from
   // ormconfig.json / ormconfig.js / ormconfig.yml / ormconfig.env / ormconfig.xml
   // files, or from special environment variables
   //
-  // to connect using configuation file, uncomment the following:
+  // TODO: to connect using configuation file, uncomment the following:
   const connection: Connection = await createConnection();
 
   // !! DO NOT PUT CONNECTION Information here. It should not be commited to your repository.
-  // to manually connect to database. uncomment the following:
-  // const connection: Connection = await createConnection({
-  //   type: process.env.TYPEORM_CONNECTION,
+  // TODO: to manually connect to database. uncomment the following:
+  // const connectionManager = getConnectionManager();
+  // const connection: Connection = connectionManager.create({
+  //   type: process.env.TYPEORM_CONNECTION as any,
   //   host: process.env.TYPEORM_HOST,
-  //   port: process.env.TYPEORM_PORT,
+  //   port: Number(process.env.TYPEORM_PORT),
   //   username: process.env.TYPEORM_USERNAME,
   //   password: process.env.TYPEORM_PASSWORD,
   //   database: process.env.TYPEORM_DATABASE,
+  //   entities: Object.values(Entities),
+  //   logging: true,
   // });
+  // await connection.connect();
+  // // await connection.synchronize(); // TODO: uncomment this line to synchronize
 
   return connection;
 };
--- src/pingDb.ts
+++ src/pingDb.ts
@@ -1,10 +1,6 @@
-import { getConnectionOptions } from "typeorm";
 import getDbConnection from "./getDbConnection";
 const pingDb = async () => {
-  const connOptions = await getConnectionOptions();
   const connection = await getDbConnection();
-
-  console.log(JSON.stringify(connOptions, null, 4));
   console.log("✔ successfully connected to database");
   connection.close();
 };
